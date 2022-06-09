942646c42c39b3d77d172e71594bdfe3a7832b05
Rodrigo Viturino <viturino_souza@outlook.com>
2022-06-09 17:41:11 -0300

    init code

---
+++ dist/js/models/negociacao.js
@@ -0,0 +1,20 @@
+export class Negociacao {
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+    get data() {
+        const data = new Date(this._data.getTime());
+        return data;
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+}
