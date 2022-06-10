cec816bcc8fbcb37b3e6e46bd13e3045baa1529a
Rodrigo Viturino <viturino_souza@outlook.com>
2022-06-10 17:30:27 -0300

    Utilizando o ABSTRACT na class VIEW e no método TEMPLATE para quem herdar essa class, saber que é preciso utilizar o método TEMPLATE

--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -6,7 +6,4 @@ export class View {
         const template = this.template(model);
         this.elemento.innerHTML = template;
     }
-    template(model) {
-        throw new Error(`Verifique se o método template está sendo aplicado na class filha`);
-    }
 }
